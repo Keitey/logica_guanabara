d71bbc67b82665f4b2bb769a933c9493df3eb02b
Keitey <hkmdev22@gmail.com>
2022-09-05 19:31:24 -0300

    add aula 6

---
+++ aula06.js
@@ -0,0 +1,17 @@
+//estrutura de repetição, contar de 0 a 10
+let i = 0;
+for( let i = 0; i < 10; i++ ){
+    console.log('contando')
+}
+
+//contar de 10 a 0
+for(let c = 10; c> 0; c--){
+    console.log('regredindo')
+}
+//fatorial
+let fat = 7;
+let res = fat
+for(i=1; i< fat; i++){
+    res *=i;
+}
+console.log(res)
